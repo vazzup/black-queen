49040a685378744ab0be20e27d2a28574616eb3f
Shyam Mehta <s4shyam95@gmail.com>
2020-03-31 23:47:14 -0400

    UI and text changes to the source app.

--- static/chat.js
+++ static/chat.js
@@ -2,7 +2,7 @@ $(function() {
     // When we're using HTTPS, use WSS too.
     var ws_scheme = window.location.protocol == "https:" ? "wss" : "ws";
     var chatsock = new ReconnectingWebSocket(ws_scheme + '://' + window.location.host + "/chat" + window.location.pathname);
-    
+
     chatsock.onmessage = function(message) {
         var data = JSON.parse(message.data);
         var chat = $("#chat")
@@ -17,7 +17,7 @@ $(function() {
         ele.append(
             $("<td></td>").text(data.message)
         )
-        
+
         chat.append(ele)
     };
 
