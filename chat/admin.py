80cc845fe38406a25555abefc674357e7d9a1b0b
Shyam Mehta <s4shyam95@gmail.com>
2020-04-06 01:10:29 -0400

    Update Commit

---
+++ chat/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from myproject.myapp.models import *
+
+admin.site.register(Room)
+admin.site.register(Player)
+admin.site.register(Game)
+admin.site.register(Hand)
+admin.site.register(Bid)
+admin.site.register(HandEntry)
+admin.site.register(Message)
